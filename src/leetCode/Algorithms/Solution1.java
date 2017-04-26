62e0c69ee9f786dd82732992465dee1fa86b7e46
qianzhihao <tonyqianzhihao@qq.com>
2017-04-26 16:11:46 +0800

    2017年4月26日 16:11:43

--- src/leetCode/Algorithms/Solution1.java
+++ src/leetCode/Algorithms/Solution1.java
@@ -1,23 +1,41 @@
 package leetCode.Algorithms;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author qianzhihao
- *Given an array of integers, return indices of the two 
- *numbers such that they add up to a specific target.
- *You may assume that each input would have exactly one 
- *solution, and you may not use the same element twice.
+ *		Given an array of integers, return indices of the two 
+ *		numbers such that they add up to a specific target.
+ *		You may assume that each input would have exactly one 
+ *		solution, and you may not use the same element twice.
  *
- *Example:
- *Given nums = [2, 7, 11, 15], target = 9,
- *Because nums[0] + nums[1] = 2 + 7 = 9,
- *return [0, 1].
+ *		Example:
+ *			Given nums = [2, 7, 11, 15], target = 9,
+ *			Because nums[0] + nums[1] = 2 + 7 = 9,
+ *			return [0, 1].
+ *
+ * * 	Java Map ���������飺
+ * 		 Map �ṩ��һ����ͨ�õ�Ԫ�ش洢������ Map ���������ڴ洢Ԫ�ضԣ�����ÿ��
+ * 		��ӳ�䵽һ��ֵ���Ӹ����϶��ԣ����Խ� List �����Ǿ�����ֵ���� Map ����ʵ��
+ * 		�ϣ����� List �� Map ���ڶ��� java.util ���⣬���߲�û��ֱ�ӵ���ϵ��
+
  */
 
 public class Solution1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public int[] twoSum(int[] numbers, int target) {
+		int[] result = new int[2];
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		for (int i = 0; i < numbers.length; i++) {
+			if (map.containsKey(target - numbers[i])) {
+				result[1] = i + 1;
+				result[1] = map.get(target - numbers[i]);
+				return result;
+			}
+			map.put(numbers[i], i + 1);
+		}
+		return result;
 	}
 
 }
