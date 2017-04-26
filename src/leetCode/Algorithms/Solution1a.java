9374fa0212dd218182dc8f2006de9051b7c6409c
qianzhihao <tonyqianzhihao@qq.com>
2017-04-26 17:30:28 +0800

    2017年4月26日 17:30:26

--- src/leetCode/Algorithms/Solution1a.java
+++ src/leetCode/Algorithms/Solution1a.java
@@ -18,40 +18,22 @@ import java.util.Map;
  */
 
 public class Solution1a {
-////	函数入口，参数 numbers 为给定的数组，target 为给定的两个数字之和
-//	public int[] twoSum(int[] numbers, int target) {
-////		新建一个数组 result，用来存放最终运算结果
-//		int[] result = new int[2];
-////		新建一个 HashMap 类 map
-//		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-////		 for 循环，循环次数为给定的数组 numbers 的长度
-//		for (int i = 0; i < numbers.length; i++) {
-////			map.containsKey：判断 map 集合对象中是否包含指定的键名
-//			if (map.containsKey(target - numbers[i])) {
-//				result[1] = i + 1;
-////				map.get 返回指定键所映射的值
-//				result[0] = map.get(target - numbers[i]);
-//				return result;
-//			}
-////			 map.put
-////         1.检查 key 是否为空；
-////			2. 计算 key 的 hashcode 和在table 里面的 index；
-////			3. 找到 table 上面的元素； 
-////			4. 遍历链表，如果没有就put 进去，有就更新；
-//			map.put(numbers[i], i + 1);
-//		}
-//		return result;
-//	}
 
 //	函数入口，参数 nums 为给定的数组，target 为给定的两个数字之和
 	public int[] twoSum(int[] nums, int target) {
+//		新建一个 HashMap 类 tracker
 		HashMap<Integer, Integer> tracker = new HashMap<Integer, Integer>();
+//		新建一个 int 变量 len ，用来记录 nums 的长度
 		int len = nums.length;
+//		 for 循环，循环次数为 nums 的长度
 		for (int i = 0; i < len; i++) {
+//			 if 判断，若 tracker 中含有 nums[i] 这个值则运行以下两行代码
 			if (tracker.containsKey(nums[i])) {
 				int left = tracker.get(nums[i]);
 				return new int[]{left+1, i+1};
-			} else {
+			} 
+//			tracker 中不含有 nums[i] 这个值则运行以下一行代码
+			else {
 				tracker.put(target - nums[i], i);
 			}
 		}
