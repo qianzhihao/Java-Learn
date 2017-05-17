8ff6cfd63eb0e0c69cf6ad67c004cd0cd0a2f776
qianzhihao <tonyqianzhihao@qq.com>
2017-05-17 11:28:33 +0800

    算法图解：二分法 2017.05.17

---
+++ src/算法图解/BinarySearch/Test.java
@@ -0,0 +1,21 @@
+package 算法图解.BinarySearch;
+
+public class Test {
+
+	public static void main(String[] args) {
+		int[] list = {2, 4, 7, 10, 11, 45, 50, 59, 60, 66, 69, 70, 79};
+		int i = BinarySearch.binarySearch(list, 2);
+		System.out.println(i);
+		
+		int j = BinarySearch.binarySearch(list, 11);
+		System.out.println(j);
+		int k = BinarySearch.binarySearch(list, 12);
+		System.out.println(k);
+		int l = BinarySearch.binarySearch(list, 1);
+		System.out.println(l);
+		int m = BinarySearch.binarySearch(list, 3);
+		System.out.println(m);
+
+	}
+
+}
