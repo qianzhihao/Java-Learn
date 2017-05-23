afe1d39f56976abade0b9fdb9f6f238d1dab0447
qianzhihao <tonyqianzhihao@qq.com>
2017-05-23 16:12:24 +0800

    LeetCode#15：3Sum 2017年5月23日 16:12:22

--- src/leetCode/Algorithms/Solution15.java
+++ src/leetCode/Algorithms/Solution15.java
@@ -22,27 +22,36 @@ import java.util.List;
 public class Solution15 {
 
 	public List<List<Integer>> threeSum(int[] num) {
-		// 对数组中的数字按照从小到大的顺序进行排序；
+		// 对数组 num 中的数字按照从小到大的顺序进行排序；
 		Arrays.sort(num);
-		
+		// 新建一个链表 res;
 		List<List<Integer>> res = new LinkedList<>();
+		// for 循环，循环的次数是 num 的大小减去2；
 		for (int i = 0; i < num.length - 2; i++) {
+			// 如果 i 等于 0 或者是数组 num 中第（i+1）个数字和第 i 个数字不相等，则执行一下语句；
 			if (i == 0 || (i > 0 && num[i] != num[i-1])) {
+				// 新建三个整数，lo 初始化为（i+1），hi初始化为数组 num 的长度减1，sum 初始化为数组 num 中最后一个数的相反数；
 				int lo = i + 1;
 				int hi = num.length - 1;
 				int sum = 0 - num[i];
+				// while 循环，循环条件是 lo 小于 hi；
 				while (lo < hi) {
+					// 如果 num 中第（lo+1）个数加上第（hi+1）个数等于 sum ，则执行以下语句；
 					if (num[lo] + num[hi] == sum) {
+						// 将数组 num 中第（i+1）个数，第（lo+1）个数和第（hi+1）个数添加到链表 res 中；
 						res.add(Arrays.asList(num[i], num[lo], num[hi]));
+						// while 循环，循环条件是 lo 小于 hi 且数组 num 中第（lo+1）个数和第（lo+2）个数相等；
 						while (lo < hi && num[lo] == num[lo+1]) {
 							lo++;
 						}
+						// while 循环，循环条件是 lo 小于 hi 且数组 num 中第（hi+1）个数和第 hi 个数相等；
 						while (lo < hi && num[hi] == num[hi-1]) {
 							hi--;
 						}
 						lo++;
 						hi--;
 					}
+					// 如果 num 中第（lo+1）个数加上第（hi+1）个数小于 sum ，则执行以下语句；
 					else if (num[lo] + num[hi] < sum) 
 						lo++;
 					else 
@@ -50,6 +59,7 @@ public class Solution15 {
 				}
 			} 
 		}
+		// 返回链表 res，即求得的结果；
 		return res;
 	}
 }
